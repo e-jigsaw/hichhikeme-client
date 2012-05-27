e263afbc8eb37e4489822c6fc4c4b0e5aed1f8c7
Takaya Kobayashi <jigsaw@live.jp>
2012-05-27 18:24:54 +0900

    fix button

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,10 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	$.support.cors = true;
 	$.mobile.allowCrossDomainPages = true;
+	$("#find").live("click", function(e) {
+		$("#success").append("Clicked");
+		$("form").submit();
+	});
 }
 
 (function() {
