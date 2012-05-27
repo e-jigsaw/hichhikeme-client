bb62e82d3d86c3e5d441ff862bbe6648d7c4f00b
Takaya Kobayashi <jigsaw@live.jp>
2012-05-27 18:12:18 +0900

    delete ajax

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,23 +3,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	$.support.cors = true;
 	$.mobile.allowCrossDomainPages = true;
-	$("#find").live("click", function(e) {
-		$("#success").append("Clicked");
-		var sendData = {
-			id: "test",
-			from: $("#hitchFrom").val(),
-			to: $("#hitchTo").val(),
-			description: $("#hitchDesc").val()
-		};
-		$.ajax({
-			url: "http://hichhikeme.herokuapp.com/post", 
-			type: "POST",
-			data: sendData, 
-			success: function(e) {
-				$("#success").append("Success!");
-			}
-		});
-	});
 }
 
 (function() {
